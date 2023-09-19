935414449f987c8a9562d82dc25bf8e33c52eb5a
Adrien Cros <adrien.cros26@gmail.com>
2023-09-19 14:26:10 +0200

    optimisation du js + créations des boutons en dynamiques suivant les catégories présentes dans l'API

--- FrontEnd/js/script.js
+++ FrontEnd/js/script.js
@@ -21,91 +21,68 @@ if (gallery === null){
    categories = JSON.parse(categories);
 }
 
-//créé les figures qui contiennent la galerie sur le site
-function generateGallery(gallery){
-    for (let i = 0; i < gallery.length; i++) {
-        //récupération de l'index de la galerie correspondante
-        const work = gallery[i];
-        //création de la figure qui contient l'image et le texte
-        const figure = document.createElement("figure");       
-        //création de l'image et de son alt
-        const imageElement = document.createElement("img");
-        imageElement.src = work.imageUrl;
-        imageElement.alt = work.title;
-        //création du texte de l'image
-        const figCaption = document.createElement("figcaption");
-        figCaption.innerText = work.title;
-
-        //ajout de l'image et du texte dans la figure
-        figure.appendChild(imageElement);
-        figure.appendChild(figCaption);    
-        //ajout de la figure contenant toutes les infos dans la div class .gallery
-        galleryDisplay.appendChild(figure)
-     }
-}
-
 //affichage de la galerie dans la page html
 generateGallery(gallery);
+//affichage des boutons suivant les catégories dans la page html
+createFilterButtons(categories);
 
-//regarder le categoryId de chaque éléments dans ma liste gallery, comparer leurs categoryId à ceux dans categories.id
-const filterAll = document.getElementById("filter-all");
-const filterObjects = document.getElementById("filter-objects");
-const filterAppartments = document.getElementById("filter-appartments");
-const filterHotels = document.getElementById("filter-hotels");
+//créé les figures qui contiennent la galerie sur le site
+function generateGallery(gallery) {
+   galleryDisplay.innerHTML = "";
+   //pour chaque éléments présent dans la gallery, créé une figure lui correspondant
+   gallery.forEach(work => {
+      //création de la figure qui contient l'image et le texte
+      const figure = document.createElement("figure");       
+      //création de l'image et de son alt
+      const imageElement = document.createElement("img");
+      imageElement.src = work.imageUrl;
+      imageElement.alt = work.title;
+      //création du texte de l'image
+      const figCaption = document.createElement("figcaption");
+      figCaption.innerText = work.title;
 
-filterAll.addEventListener("click", () => {
-   resetFilter(gallery, categories);
-})
-
-filterObjects.addEventListener("click", () => {
-   filterByObjects(gallery, categories);
-})
-
-filterAppartments.addEventListener("click", () => {
-   filterByAppartments(gallery, categories);
-})
-
-filterHotels.addEventListener("click", () => {
-   filterByHotels(gallery, categories);
-})
-
-//A FAIRE FONCTION NO FILTER
-function resetFilter (gallery) {
-   document.querySelector(".gallery").innerHTML = "";
-   generateGallery(gallery);
+      //ajout de l'image et du texte dans la figure
+      figure.appendChild(imageElement);
+      figure.appendChild(figCaption);    
+      //ajout de la figure contenant toutes les infos dans la div class .gallery
+      galleryDisplay.appendChild(figure);
+   });
 };
 
-//A FAIRE FONCTION TRIER PAR OBJETS 
-function filterByObjects (gallery, categories) {
-   const filter = gallery.filter(function (gallery) {
-      if (gallery.categoryId === 1) {
-         return gallery;
-      }
-  });
-  document.querySelector(".gallery").innerHTML = "";
-  generateGallery(filter);
+function createFilterButtons (categories) {
+   //selectionne le parent avec la classe filter
+   const filterContainer = document.querySelector(".filter")
+   //création du bouton "Tous" avec le data-attribute défini sur 0
+   const button = document.createElement('button');
+   button.classList.add("filter-btn");
+   button.textContent = "Tous";
+   button.setAttribute('data-category', 0);
+   filterContainer.appendChild(button);
+   //pour chaque éléments présent dans la categories, créé un bouton lui correspondant
+   categories.forEach(category => {
+      const button = document.createElement('button');
+      button.classList.add("filter-btn");
+      button.textContent = category.name;
+      button.setAttribute('data-category', category.id);
+      filterContainer.appendChild(button);
+    }); 
 };
 
-//A FAIRE FONCTION TRIER PAR APPARTEMENTS
-function filterByAppartments (gallery, categories) {
-   const filter = gallery.filter(function (gallery) {
-      if (gallery.categoryId === 2) {
-         return gallery;
-      }
-  });
-  document.querySelector(".gallery").innerHTML = "";
-  generateGallery(filter);
-};
+//boutons de filtre
+const filterButtons = document.querySelectorAll('[data-category]');
 
-//A FAIRE FONCTION TRIER PAR HOTEL/RESTAU
-function filterByHotels (gallery, categories) {
-   const filter = gallery.filter(function (gallery) {
-      if (gallery.categoryId === 3) {
-         return gallery;
-      }
-  });
-  document.querySelector(".gallery").innerHTML = "";
-  generateGallery(filter);
-};
+// sélectionne tous les boutons avec l'attribut 'data-category'
+filterButtons.forEach(button => {
+   // ajoute un event listener sur chaque bouton
+   button.addEventListener('click', () => {
+      // récupère la valeur de l'attribut 'data-category' et la convertit en nombre
+      const category = parseInt(button.getAttribute('data-category'));
 
-//suite et test pour voir si les commit marchent
+     // détermine si la catégorie sélectionnée est "Tous" (0) ou une autre catégorie
+     // si c'est "Tous" (0) affiche tout, sinon filtre par catégorie
+     const filter = category === 0 ? gallery : gallery.filter(work => work.categoryId === category);
+     
+     // affiche seulement les objets filtrés ou tout les objets si data-category était de 0
+     generateGallery(filter);
+   });
+});
