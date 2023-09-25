3f0be02218e4fa9cb28460e267bbf393c08eb03b
Adrien Cros <adrien.cros26@gmail.com>
2023-09-25 16:18:52 +0200

    Modification structure JS + ajout du logout + début d'ajout de la modal avec premier visuel

--- FrontEnd/js/login.js
+++ FrontEnd/js/login.js
@@ -1,45 +1,33 @@
-//récupération du formulaire
 const form = document.querySelector('form');
+const returnMainPage = document.getElementById("projects");
 
-//traitement des infos lors du click sur le bouton se connecter
 form.addEventListener("submit", (event) => {
-    //empeche l'application par defaut de submit
     event.preventDefault();
-    //récupération des informations de connection
     const email = document.getElementById("email").value;
     const password = document.getElementById("password").value;
-    //vérifie si l'email a le bon format mail (XXX @ XXX . XXX)
     const regex = new RegExp("[a-z0-9._-]+@[a-z0-9._-]+\.[a-z0-9._-]+");
     const emailCheck = regex.test(email);
     if (emailCheck !== false) {
-        //création d'un objet qui contient les informations de connection
         const comboLogin = {
             email: email,
             password: password
         }
-        //transformation en string de l'objet
         const infoToSubmit = JSON.stringify(comboLogin);
         
-        //envoi de l'objet dans l'API
         fetch("http://localhost:5678/api/users/login", {
             method: "POST",
             headers: { "Content-Type": "application/json" },
             body: infoToSubmit
         })
         .then(response => {
-            //vérifier le code de statut
             if (!response.ok) {
                 throw new Error('La demande a échoué avec le code ' + response.status);
             }
-            //convertir la réponse en JSON
             return response.json();
         })
         .then(data => {
-            //stocke les données reçu du serveur si le code de réponse est 200
             const login = JSON.stringify(data);
-            //stocke les informations de l'utilisateur connecté en session storage (pour reset les infos lorsque l'utilisateur ferme son navigateur)
             window.sessionStorage.setItem("login", login);
-            //redirige vers la page principale
             location.href ="./index.html";  
         })
         .catch(error => {
@@ -51,4 +39,8 @@ form.addEventListener("submit", (event) => {
         const errorMsg = document.querySelector(".error-msg");
         errorMsg.textContent = "Format d'E-mail invalide.";
     }
-})
+});
+
+returnMainPage.addEventListener("click", () =>{
+    location.href ="./index.html";
+});
--- FrontEnd/js/script.js
+++ FrontEnd/js/script.js
@@ -1,80 +1,78 @@
-//Récupération de la class .gallery
 const galleryDisplay = document.querySelector(".gallery");
-
-//Récupération des infos stockées dans le localStorage si deja existant
+const buttonAll = 0;
 let gallery = window.localStorage.getItem('gallery');
 let categories = window.localStorage.getItem('categories');
-if (gallery === null){
-   // Récupération depuis l'API
-   let response = await fetch('http://localhost:5678/api/works');
-   gallery = await response.json();
-   response = await fetch('http://localhost:5678/api/categories');
-   categories = await response.json();
-   // Transformation des éléments en JSON
-   const galleryNumbers = JSON.stringify(gallery);
-   const categoriesNumbers = JSON.stringify(categories);
-   // Stockage des informations dans le localStorage
-   window.localStorage.setItem("gallery", galleryNumbers);
-   window.localStorage.setItem("categories", categoriesNumbers);
-} else {
-   gallery = JSON.parse(gallery);
-   categories = JSON.parse(categories);
-}
 
-//récupère les informations de session storage dans la key login si l'utilisateur est loggé
-const userInfo = JSON.parse(window.sessionStorage.getItem('login'));
-//si l'utilisateur est loggé, alors affiche ou enlève les infos d'edition de la page
-if (userInfo) {
-   console.log(userInfo);
-   //affiche les barres d'edition de la page
-   const editionDisplayElements = document.querySelectorAll(".display-login");
-   //itère sur chaque éléments de editionDisplayElements
-   for (const element of editionDisplayElements) {
-      element.style.display = "flex";
+init();
+displayAdminUI();
+
+async function init() {
+   if (gallery === null){
+      let response = await fetch('http://localhost:5678/api/works');
+      gallery = await response.json();
+      response = await fetch('http://localhost:5678/api/categories');
+      categories = await response.json();
+      const galleryNumbers = JSON.stringify(gallery);
+      const categoriesNumbers = JSON.stringify(categories);
+      window.localStorage.setItem("gallery", galleryNumbers);
+      window.localStorage.setItem("categories", categoriesNumbers);
+   } else {
+      gallery = JSON.parse(gallery);
+      categories = JSON.parse(categories);
    }
-   //enlève la barre de filtre
-   const editionFilterBar = document.querySelector(".filter");
-   editionFilterBar.style.display = "none";
+
+   generateGallery(gallery);
+   createFilterButtons(categories);
+};
+
+function displayAdminUI() {
+   const userInfo = JSON.parse(window.sessionStorage.getItem('login'));
+   const loginText = document.getElementById("login-btn");
+   if (userInfo !== null) {
+      if (userInfo.userId === 1) {
+         console.log(userInfo);
+         const editionDisplayElements = document.querySelectorAll(".display-login");
+         for (const element of editionDisplayElements) {
+            element.style.display = "flex";
+            loginText.innerText = "logout";
+         }
+         const editionFilterBar = document.querySelector(".filter");
+         editionFilterBar.style.display = "none";
+      } else {
+         loginText.innerText = "logout";
+         console.log("User isn't an admin.");
+      }
+   } else {
+      console.log("User not logged.");
+   }
 }
 
-//affichage de la galerie dans la page html
-generateGallery(gallery);
-//affichage des boutons suivant les catégories dans la page html
-createFilterButtons(categories);
-
-//créé les figures qui contiennent la galerie sur le site
 function generateGallery(gallery) {
    galleryDisplay.innerHTML = "";
-   //pour chaque éléments présent dans la gallery, créé une figure lui correspondant
    gallery.forEach(work => {
-      //création de la figure qui contient l'image et le texte
       const figure = document.createElement("figure");       
-      //création de l'image et de son alt
       const imageElement = document.createElement("img");
       imageElement.src = work.imageUrl;
       imageElement.alt = work.title;
-      //création du texte de l'image
       const figCaption = document.createElement("figcaption");
       figCaption.innerText = work.title;
 
-      //ajout de l'image et du texte dans la figure
       figure.appendChild(imageElement);
       figure.appendChild(figCaption);    
-      //ajout de la figure contenant toutes les infos dans la div class .gallery
+
       galleryDisplay.appendChild(figure);
    });
 };
 
 function createFilterButtons (categories) {
-   //selectionne le parent avec la classe filter
    const filterContainer = document.querySelector(".filter")
-   //création du bouton "Tous" avec le data-attribute défini sur 0
+
    const button = document.createElement('button');
    button.classList.add("filter-btn");
    button.textContent = "Tous";
-   button.setAttribute('data-category', 0);
+   button.setAttribute('data-category', buttonAll);
    filterContainer.appendChild(button);
-   //pour chaque éléments présent dans la categories, créé un bouton lui correspondant
+
    categories.forEach(category => {
       const button = document.createElement('button');
       button.classList.add("filter-btn");
@@ -84,27 +82,57 @@ function createFilterButtons (categories) {
     }); 
 };
 
-//boutons de filtre
 const filterButtons = document.querySelectorAll('[data-category]');
-
-// sélectionne tous les boutons avec l'attribut 'data-category'
 filterButtons.forEach(button => {
-   // ajoute un event listener sur chaque bouton
    button.addEventListener('click', () => {
-      // récupère la valeur de l'attribut 'data-category' et la convertit en nombre
       const category = parseInt(button.getAttribute('data-category'));
-
-     // détermine si la catégorie sélectionnée est "Tous" (0) ou une autre catégorie
-     // si c'est "Tous" (0) affiche tout, sinon filtre par catégorie
-     const filter = category === 0 ? gallery : gallery.filter(work => work.categoryId === category);
-     
-     // affiche seulement les objets filtrés ou tout les objets si data-category était de 0
-     generateGallery(filter);
+      const filter = category === buttonAll ? gallery : gallery.filter(work => work.categoryId === category);
+      generateGallery(filter);
    });
 });
 
-//bouton de login
 const boutonLogin = document.getElementById("login-btn");
 boutonLogin.addEventListener("click", () => {
+   const userInfo = JSON.parse(window.sessionStorage.getItem('login'));
+   if (userInfo) {
+      window.sessionStorage.removeItem('login');
+   }
    location.href ="./login.html";
 });
+
+/**************MODAL**********************/
+const boutonModify = document.querySelector(".modify-btn");
+const modal = document.getElementById("modal");
+const modalCloseImg = document.querySelector(".modal-close");
+
+function createModalGallery(gallery) {
+   const modalGallery = document.querySelector(".modal-gallery");
+   modalGallery.innerHTML = "";
+   gallery.forEach(work => {
+      const imageContainer = document.createElement("div");
+      imageContainer.classList.add("modal-img-container");
+      imageContainer.setAttribute('data-work-id', work.id);
+
+      const imageElement = document.createElement("img");
+      imageElement.src = work.imageUrl;
+
+      const deleteElement = document.createElement("i");
+      deleteElement.classList.add("modal-delete-item");
+      deleteElement.classList.add("fa-solid");
+      deleteElement.classList.add("fa-trash-can");
+
+      imageContainer.appendChild(imageElement);
+      imageContainer.appendChild(deleteElement);
+
+      modalGallery.appendChild(imageContainer);
+   });
+};
+
+boutonModify.addEventListener("click", () => {
+   modal.style.display = "flex";
+   createModalGallery(gallery);
+});
+
+modalCloseImg.addEventListener("click", () => {
+   modal.style.display = "none";
+})
